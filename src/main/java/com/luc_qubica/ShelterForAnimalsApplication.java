caa228c1e07aaf8f01ebc256ce8939d63582a750
Wolf <kubica_lukasz@wp.pl>
2021-09-28 19:45:58 +0200

    Initial commit

---
+++ src/main/java/com/luc_qubica/ShelterForAnimalsApplication.java
@@ -0,0 +1,13 @@
+package com.luc_qubica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterForAnimalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterForAnimalsApplication.class, args);
+	}
+
+}
